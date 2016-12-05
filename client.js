c6a044d532001e0bead40e95015395e46a6d9509
Karla Hugo <karlabrandon817@gmail.com>
2016-12-04 21:10:33 -0600

    simplified code

--- client.js
+++ client.js
@@ -37,29 +37,17 @@ $(document).ready(function() {
     //next button function
     $('#nextButton').on('click', function() {
         i = i + 1;
-        i = i % tauStudentArr.length; // if we've gone too high, start from `0` again
-        //show student name and info on DOM
-        outputText = '<p>' + '<strong>' + "Name:" + '</strong>' + ' ' + tauStudentArr[i].first_name + ' ' + tauStudentArr[i].last_name + '</p>' +
-            '<img class="picture" src="' + tauStudentArr[i].picUrl + '">' +
-            '<p>' + '<strong>' + "About" + ' ' + tauStudentArr[i].first_name + ":" + '' + '</strong>' + ' ' + tauStudentArr[i].info + '</p>' +
-            '<p class="dispStud">' + 'Displaying Student:' + ' ' + (tauStudentArr.indexOf(tauStudentArr[i]) + 1) + ' ' + 'out of' + ' ' + tauStudentArr.length;
-        $('#tauStudents').html(outputText);
-        console.log(tauStudentArr[i]);
+        i = i % tauStudentArr.length;
+        displayStudents();
     }); //end next button on click function
 
     //previous button function
     $('#prevButton').on('click', function() {
-        if (i === 0) { // i would become 0
+        if (i === 0) {
             i = tauStudentArr.length;
         }
-        i = i - 1; // decrease by one
-        //show student name and info on DOM
-        outputText = '<p>' + '<strong>' + "Name:" + '</strong>' + ' ' + tauStudentArr[i].first_name + ' ' + tauStudentArr[i].last_name + '</p>' +
-            '<img class="picture" src="' + tauStudentArr[i].picUrl + '">' +
-            '<p>' + '<strong>' + "About" + ' ' + tauStudentArr[i].first_name + ":" + '' + '</strong>' + ' ' + tauStudentArr[i].info + '</p>' +
-            '<p class="dispStud">' + 'Displaying Student:' + ' ' + (tauStudentArr.indexOf(tauStudentArr[i]) + 1) + ' ' + 'out of' + ' ' + tauStudentArr.length;
-        $('#tauStudents').html(outputText);
-        console.log(tauStudentArr[i]);
+        i = i - 1;
+        displayStudents();
     }); //end prev button on click function
 
 
